cd6086a6b94f6b79349f75e24723243b182b3d12
Ahmed Shaaban <ashaaban0109@gmail.com>
2024-02-07 04:06:01 +0000

    Added more informative toasts.

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
 let TARGET_WORD = "apple";
 let ROWS = []
 let GAME_MODE = 5  // play game with x letter words
-
 let guessNumber = 0;
+
+function startNewGame(columns = 5, rows = 6) {
+    ROWS = []
+    GAME_MODE = columns
+    guessNumber = 0;
+    populateGrid(columns, rows)
+    TARGET_WORD = generateRandomWord(GAME_MODE)
+
+    const inputField = document.getElementById("guess");
+    inputField.maxLength = GAME_MODE;
+}
+
+function populateGrid(columns = 5, rows = 6) {
+    let grid =  document.getElementById("grid-container")
+    clearGrid(grid)
+    for (let i = 0; i < rows; i++) {
+        let row = document.createElement("div")
+        row.classList.add("guess-row")
+        grid.append(row)
+        ROWS.push(row)
+        for (let j = 0; j < columns; j++) {
+            let cell = document.createElement("div");
+            cell.classList.add("guess-cell")
+            row.append(cell)
+        }
+    }
+}
+
+function clearGrid(grid) {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+}
+
+function showToast(message) {
+    Toastify({
+        text: message,
+        duration: 5000, // Duration in milliseconds
+        gravity: "top", // toast position
+        position: 'right', // toast position
+      }).showToast();
+}
 
 function submitGuess() {
     const guessInput = document.getElementById("guess");
     const guess = guessInput.value.trim().toUpperCase();
 
-    if (guess.length !== GAME_MODE || !/^[A-Z]+$/.test(guess)) {
-        showToast(`Please enter a ${GAME_MODE}-letter word containing only alphabets.`);
+    if (!isAcceptableGuess(guess)) {
         return;
     }
 
@@ -32,6 +72,29 @@ function submitGuess() {
     guessNumber++
 }
 
+function isAcceptableGuess(guess) {
+    if (guess.length !== GAME_MODE && !/^[A-Z]+$/.test(guess)) {
+        showToast(`Please enter a ${GAME_MODE}-letter word containing only alphabets.`);
+        return false
+    } else if (guess.length !== GAME_MODE) {
+        showToast(`Please enter a ${GAME_MODE}-letter word.`);
+        return false
+    } else if (!/^[A-Z]+$/.test(guess)) {
+        showToast(`Please enter a word containing only alphabets.`);
+        return false
+    } else if (!isRealWord(guess)) {
+        showToast(`Please enter a real word.`);
+        return false
+    } else {
+        return true
+    }
+}
+
+// TODO
+function isRealWord(guess) {
+    return true
+}
+
 function getCellColor(letter, position) {
     if (letter === TARGET_WORD[position].toUpperCase()) {
         return "#5cb85c"; // Dark green
@@ -40,39 +103,6 @@ function getCellColor(letter, position) {
     } else {
         return "#d9534f"; // Light red
     }
-}
-
-function clearGrid(grid) {
-    while (grid.firstChild) {
-        grid.removeChild(grid.firstChild);
-    }
-}
-
-function populateGrid(columns = 5, rows = 6) {
-    let grid =  document.getElementById("grid-container")
-    clearGrid(grid)
-    for (let i = 0; i < rows; i++) {
-        let row = document.createElement("div")
-        row.classList.add("guess-row")
-        grid.append(row)
-        ROWS.push(row)
-        for (let j = 0; j < columns; j++) {
-            let cell = document.createElement("div");
-            cell.classList.add("guess-cell")
-            row.append(cell)
-        }
-    }
-}
-
-function startNewGame(columns = 5, rows = 6) {
-    ROWS = []
-    GAME_MODE = columns
-    guessNumber = 0;
-    populateGrid(columns, rows)
-    TARGET_WORD = generateRandomWord(GAME_MODE)
-
-    const inputField = document.getElementById("guess");
-    inputField.maxLength = GAME_MODE;
 }
 
 // TODO
@@ -97,15 +127,6 @@ function revealOptimalWord() {
     let expectedInformation = 3.14
 
     showToast(`Optimal word: ${optimalWord} \nExpected Information: ${expectedInformation}`)
-}
-
-function showToast(message) {
-    Toastify({
-        text: message,
-        duration: 5000, // Duration in milliseconds
-        gravity: "top", // toast position
-        position: 'right', // toast position
-      }).showToast();
 }
 
 startNewGame()
